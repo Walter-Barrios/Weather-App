04126d7e55c0d390339a51b6bff245c13ac66e16
Walter Barrios <walterbarrios@protonmail.com>
2022-05-14 16:05:46 -0300

    add styled-components

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,19 +1,36 @@
 import React from 'react';
 import s from './Card.module.css';
+import styled, { css } from 'styled-components';
+
+const center = css`
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+`;
+
+const CardStyle = styled.div`
+  width: 25vw;
+  ${center}
+`;
 
 function Card({max,min,name,img,onClose}) {
   return (
-    <article className={s.fondo}>
-        <button onClick={onClose}>X</button>
-        <h2>{name}</h2>
-        <h3>Min</h3>
-        {min}
-        <h3>Max</h3>
-        {max}
-        <figure>
-            <img src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt="weather img" />
-        </figure>
-    </article>
+    <CardStyle>
+      <article className={s.fondo}>
+          <button onClick={onClose}>X</button>
+          <h2>{name}</h2>
+          <h3>Min</h3>
+          {min}
+          <h3>Max</h3>
+          {max}
+          <figure>
+              <img src={`http://openweathermap.org/img/wn/${img}@2x.png`} alt="weather img" />
+          </figure>
+      </article>      
+    </CardStyle>
+
   )
 }
 
--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import styled from 'styled-components';
+
+const DivWrapper = styled.div`
+  background-color: value(--primary-bg-color);
+`;
 
 function SearchBar({onSearch}) {
   return (
-    <div>
+    <DivWrapper>
       <input type="text" placeholder='Ciudad...' />
       <button onClick={onSearch}>Buscar</button>
-    </div>
+    </DivWrapper>
   )
 }
 
