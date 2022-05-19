2088aac63cbd786adcbc3aeb89cf8d1cd997d68b
Walter Barrios <walterbarrios@protonmail.com>
2022-05-18 22:25:25 -0300

    fix sweetalert button text color

--- src/services/fetchCity.jsx
+++ src/services/fetchCity.jsx
@@ -4,54 +4,61 @@ import withReactContent from 'sweetalert2-react-content'
 const MySwal = withReactContent(Swal)
 
 function fetchCity(city, setCities) {
-    const apiKey = process.env.REACT_APP_API_KEY;
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`,)
-    .then((response) => response.json())
-    .then((resource) => {
-        if(resource.main !== undefined) {
-            const city = {
-                min: Math.round(resource.main.temp_min),
-                max: Math.round(resource.main.temp_max),
-                img: resource.weather[0].icon,
-                id: resource.id,
-                wind: resource.wind.speed,
-                temp: resource.main.temp,
-                name: resource.name,
-                weather: resource.weather[0].main,
-                clouds: resource.clouds.all,
-                latitud: resource.coord.lat,
-                longitud: resource.coord.lon,                
-            };
-            setCities( oldCities => {
-                if(oldCities.some(c => c.name === city.name)) {
-                    MySwal.fire({
-                        title: `${city.name}`,
-                        text: 'That city is already found.',
-                        icon: 'info',
-                        confirmButtonText: 'Cool',
-                        confirmButtonColor: 'yellow',
-                        customClass: {
-                            confirmButton: 'black'
-                        },
-                    })
-                    return oldCities;
-                }else {
-                    return [...oldCities, city];
-                }
-            })
-        }else {
-            MySwal.fire({
-                title: `"${city}"`,
-                text: 'City not found.',
-                icon: 'error',
-                confirmButtonText: 'Ok',
-                confirmButtonColor: 'yellow',
-                customClass: {
-                    confirmButton: 'black'
-                },
-            })
-        }
-    });
+
+    if(city) {
+        const apiKey = process.env.REACT_APP_API_KEY;
+        fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`,)
+        .then((response) => response.json())
+        .then((resource) => {
+            if(resource.main !== undefined) {
+                const city = {
+                    min: Math.round(resource.main.temp_min),
+                    max: Math.round(resource.main.temp_max),
+                    img: resource.weather[0].icon,
+                    id: resource.id,
+                    wind: resource.wind.speed,
+                    temp: resource.main.temp,
+                    name: resource.name,
+                    weather: resource.weather[0].main,
+                    clouds: resource.clouds.all,
+                    latitud: resource.coord.lat,
+                    longitud: resource.coord.lon,                
+                };
+                setCities( oldCities => {
+                    if(oldCities.some(c => c.name === city.name)) {
+                        MySwal.fire({
+                            customClass: {
+                               confirmButton: 'black'
+                            },
+                            title: `${city.name}`,
+                            text: 'That city is already found.',
+                            icon: 'info',
+                            confirmButtonText: `<span style="color:black">Cool<span>`,
+                            confirmButtonColor: 'yellow'
+                        })
+                        return oldCities;
+                    }else {
+                        return [...oldCities, city];
+                    }
+                })
+            }else {
+                MySwal.fire({
+                    title: `"${city}"`,
+                    text: 'City not found.',
+                    icon: 'error',
+                    confirmButtonText: `<span style="color:black">Ok<span>`,
+                    confirmButtonColor: 'yellow'
+                })
+            }
+        });
+    }else {
+        MySwal.fire({
+            title: 'You must enter a city.',
+            icon: 'warning',
+            confirmButtonText: `<span style="color:black">I get it<span>`,
+            confirmButtonColor: 'yellow'
+        })
+    }
 }
 
 export default fetchCity;
